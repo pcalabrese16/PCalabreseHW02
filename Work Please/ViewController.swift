2c188c8d8ac013277af3bb25aab0b79f6eaa0ed7
Patrick Calabrese <PatrickCalabrese@patricks-mbp.bc.edu>
2017-01-20 00:49:30 -0500

    Initial Commit

---
+++ Work Please/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Work Please
+//
+//  Created by Patrick Calabrese on 1/20/17.
+//  Copyright © 2017 Patrick Calabrese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
